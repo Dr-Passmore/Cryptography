6df05305b5ac3fbef371881ff4049e9f4f4d94f4
ClobberCreations <clobbercreations@gmail.com>
2022-09-14 19:58:55 +0100

    AB#65 Caesar Cipher encryption complete

--- Ciphers/SubsitutionCiphers/CaesarCipher.py
+++ Ciphers/SubsitutionCiphers/CaesarCipher.py
@@ -1,26 +1,48 @@
 import logging
-
+import string
 
 class caesarCipher:
-    def __init__(self):
-        logging.info('ROT13 Cipher has been initialised')
+    def __init__(self, text, step):
+        logging.info('Caesar Cipher has been initialised')
         # set up lowercase and upper case alphabets (use self.)
-        
+        self.uppercase = list(string.ascii_uppercase)
+        self.lowercase = list(string.ascii_lowercase)
+        encryptedtext = caesarCipher.encrypt(self,text,step)
+        print(encryptedtext)
+
+        # add check whether encrypt or decrypt
     
     def encrypt (self, text, step):
         output = []
         
         #add logging
-        
+        logging.info('Caesar Cipher encryption has commenced using a step of {}'.format(step))
         #loop per letter
+        for letter in text:
+            if letter in self.uppercase:
+                # index
+                index = self.uppercase.index(letter)
+                #encrypt (index + step)
+                encrypt = (index + step)%26
+                #newletter
+                newletter = self.uppercase[encrypt]
+                #append letter to output
+                output.append(newletter)
+            elif letter in self.lowercase:
+                # index
+                index = self.lowercase.index(letter)
+                #encrypt (index + step)
+                encrypt = (index + step)%26
+                #newletter
+                newletter = self.lowercase[encrypt]
+                #append letter to output
+                output.append(newletter)
+            else:
+                #append letter to output
+                output.append(letter)      
         
-        # index
         
-        #encrypt (index + step)
         
-        #newletter
-        
-        #append letter to output
         
         encryptedText = ''.join(map(str, output))
         return encryptedText
@@ -51,4 +73,7 @@ logging.basicConfig(filename='Cryptography.log',
                     format='%(asctime)s %(levelname)s %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S')
 
-caesarCipher()
+
+text = 'This is a test'
+step = 6
+caesarCipher(text,step)
