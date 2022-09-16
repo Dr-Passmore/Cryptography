76898fef99e3b0b422f587585e34497f1fc4fff2
Phillip Passmore <phillipspassmore@gmail.com>
2022-09-16 09:43:24 +0100

    AB#67 Atbash Cipher python file created
    Signed-off-by: Phillip Passmore <phillipspassmore@gmail.com>

---
+++ Ciphers/SubsitutionCiphers/AtbashCipher.py
@@ -0,0 +1,16 @@
+import string
+import logging
+import cryptography
+
+class atbashCipher:
+    def __init__(self, text, encrypt):
+        #Mirrored alphabet A = Z, Y = B. Dictionary approach would work well
+        logging.info('Atbash Cipher has been initialised')
+        
+    def encrypt (text, encrypt):
+        #only requires one. A IF statement using encyrpt True or False can be used to update logging info
+        logging.info('encrypt')
+        
+    def output(output):
+        logging.info('Returning message')
+        cryptography.CipherTool.returnOutput(output)
