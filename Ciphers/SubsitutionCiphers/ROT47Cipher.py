c64d3b0bdd76710ab5977aa25a29b1d35be6961f
Phillip Passmore <phillipspassmore@gmail.com>
2022-09-20 18:57:36 +0100

    AB#74 ROT47 __init__ function completed
    Signed-off-by: Phillip Passmore <phillipspassmore@gmail.com>

--- Ciphers/SubsitutionCiphers/ROT47Cipher.py
+++ Ciphers/SubsitutionCiphers/ROT47Cipher.py
@@ -1,3 +1,4 @@
+from re import ASCII
 import string
 import logging
 import cryptography
@@ -7,11 +8,19 @@ class ROT47Cipher:
         #Encodes all ASCII visible letters (Character 33 '!' to 126 '~') and shifts them by an index change of 47
         #"This is a test!" becomes "%9:D :D 2 E6DEP" 
         logging.info('ROT47 Cipher has been initialised')
+        self.ASCII = []
+        for character in range(32,127):
+            letter = chr(character)
+            self.ASCII.append(letter)
+        if encrypt == True:
+            ROT47Cipher.encrypt(self, text)
+        else:
+            ROT47Cipher.decrypt(self, text)
         
-    def encrypt (text, encrypt):
+    def encrypt (self, text):
         logging.info('encrypt')
         
-    def decrypt (text, encrypt):
+    def decrypt (self, text):
         logging.info('decrypt')
         
     def output(output):
@@ -23,3 +32,8 @@ logging.basicConfig(filename='Cryptography.log',
                     level=logging.INFO,
                     format='%(asctime)s %(levelname)s %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S')
+
+text = "Cat"
+encrypt = True
+
+ROT47Cipher(text, encrypt)
