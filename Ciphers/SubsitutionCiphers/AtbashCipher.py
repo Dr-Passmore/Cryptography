9e3314817fa8695e373706abc0d52c3d84ebe537
ClobberCreations <clobbercreations@gmail.com>
2022-09-21 20:24:47 +0100

    AB#67 Dictionary set up

--- Ciphers/SubsitutionCiphers/AtbashCipher.py
+++ Ciphers/SubsitutionCiphers/AtbashCipher.py
@@ -1,22 +1,42 @@
 import string
 import logging
-import cryptography
+#import cryptography
 
 class atbashCipher:
     def __init__(self, text, encrypt):
         #Mirrored alphabet A = Z, Y = B. Dictionary approach would work well
+        lowercase = list(string.ascii_lowercase)
+        lowercaseReverse = list(string.ascii_lowercase)
+        lowercaseReverse.reverse()
+        lowercaseDictionary = dict(zip(lowercase, lowercaseReverse))
+        #print(lowercaseDictionary)
+        uppercase = list(string.ascii_uppercase)
+        uppercaseReverse = list(string.ascii_uppercase)
+        uppercaseReverse.reverse()
+        uppercaseDictionary = dict(zip(uppercase, uppercaseReverse))
+
+        combinedDictionary = lowercaseDictionary.copy()
+        combinedDictionary.update(uppercaseDictionary)
+        print(combinedDictionary)
+        
         logging.info('Atbash Cipher has been initialised')
         
     def encrypt (text, encrypt):
         #only requires one. A IF statement using encyrpt True or False can be used to update logging info
+        for letter in text:
+            if 
         logging.info('encrypt')
         
     def output(output):
         logging.info('Returning message')
-        cryptography.CipherTool.returnOutput(output)
+        #cryptography.CipherTool.returnOutput(output)
         
 logging.basicConfig(filename='Cryptography.log', 
                     filemode='a', 
                     level=logging.INFO,
                     format='%(asctime)s %(levelname)s %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S')
+
+text = 'test'
+encrypt = True
+atbashCipher(text,encrypt)
