9af04457ec4b67406b61f11afb1b43701f2dec64
Phillip Passmore <phillipspassmore@gmail.com>
2022-09-23 17:41:48 +0100

    AB#81 output function added. Check for blank key
    Signed-off-by: Phillip Passmore <phillipspassmore@gmail.com>

--- Ciphers/SubsitutionCiphers/VigenèreCipher.py
+++ Ciphers/SubsitutionCiphers/VigenèreCipher.py
@@ -9,33 +9,42 @@ class VigenèreCipher:
         self.lowercase = list(string.ascii_letters)
         self.uppercase = list(string.ascii_uppercase)
         key = VigenèreCipher.keyProcessing(self, key)
-        
-        if encrypt == True:
-            output = VigenèreCipher.encrypt(self, key, text)
+        if key == None:
+            output = "Error - No key provided. The Vigenère Cipher requires letters"
+            VigenèreCipher.output(output)
         else:
-            output = VigenèreCipher.decrypt(self, key, text)
-         
-        print(key)
+            if encrypt == True:
+                output = VigenèreCipher.encrypt(self, key, text)
+            else:
+                output = VigenèreCipher.decrypt(self, key, text)
+            
+            print(key)
         
     def keyProcessing(self, key):
         logging.info("Processing the key")
         key = list(key)
-        print (key)
         updatedkey = []
         for letter in key:
-            print(letter)
             if letter in self.lowercase:
                 updatedkey.append(letter)
             else:
                 continue
-        key = updatedkey    
-        return key
+        key = updatedkey
+        if key == []:
+            logging.error("No key provided")
+        else:    
+            return key
 
     def encrypt(self, key, text):
         logging.info("Encrypting")
         
     def decrypt(self, key, text):
         logging.info("Decrypting")
+        
+    def output(output):
+        sample = output[0:30]
+        logging.info('Returning message: {}...'.format(sample))
+        cryptography.CipherTool.returnOutput(output)
 
 logging.basicConfig(filename='Cryptography.log', 
                     filemode='a', 
@@ -46,4 +55,5 @@ logging.basicConfig(filename='Cryptography.log',
 encrypt = True
 text = "This is a test"
 key = "angry!! bear!!!!12345 paws"
+
 VigenèreCipher(key, text, encrypt)
